90a1b412dfa44f25a0dc5c283dc0f9a317da407a
Arti Zirk <arti.zirk@gmail.com>
2016-09-10 00:23:14 +0300

    Formated the code with astyle

--- src/main.c
+++ src/main.c
@@ -7,12 +7,13 @@ int main (void)
 {
     /*  set pin 25 of PORTB for output*/
     DDRA |= _BV(DDA3);
-    while(1) {
+
+    while (1) {
         /*  set pin 25 high to turn led on  */
         PORTA |= _BV(PORTA3);
         _delay_ms(BLINK_DELAY_MS);
         /*  set pin 25 low to turn led off */
-        PORTA   &=  ~_BV(PORTA3);
+        PORTA &= ~_BV(PORTA3);
         _delay_ms(BLINK_DELAY_MS);
     }
 }
