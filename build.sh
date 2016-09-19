8436ba76b0d414ad650fe54c01f578b1bec6d99f
Arti Zirk <arti.zirk@gmail.com>
2016-09-19 18:01:25 +0300

    Remove unnecessary build.sh script

--- build.sh
+++
@@ -1,4 +0,0 @@
-avr-gcc -Os -DF_CPU=16000000UL -Wall -Wextra -Wpedantic -Wformat -pedantic-errors -Werror -Wfatal-errors -mmcu=atmega2560 -c -o src/main.o src/main.c
-avr-gcc -mmcu=atmega2560 src/main.o -o bin/atmega2560-user-code.elf
-avr-objcopy -O ihex -R .eeprom bin/atmega2560-user-code.elf bin/atmega2560-user-code.ihx
-avrdude -v -F -V -c stk500v2 -p m2560 -P /dev/ttyACM0 -b 115200 -U flash:w:bin/atmega2560-user-code.ihx
